dd69f12ec50400eed73d09d643ad664a020955ef
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2024-09-12 21:34:09 +0600

    Featured part with parallax has been completed

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,10 +5,10 @@ import "./Featured.css";
 
 const Featured = () => {
     return (
-        <div className="featured-item">
+        <div className="featured-item my-20 pt-8 bg-fixed">
             <SectionTitle subHeading={"Check it out"} heading={"FROM OUR MENU"} ></SectionTitle>
 
-            <div className="md:flex justify-center items-center px-8 py-16">
+            <div className="md:flex justify-center items-center pb-20 pt-12 px-36 text-white bg-slate-500 bg-opacity-40">
                 <div>
                     <img src={featuredimg} alt="" />
                 </div>
@@ -16,6 +16,7 @@ const Featured = () => {
                     <p>March 20, 2023</p>
                     <p className="uppercase">Where can I get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
+                    <button className="btn btn-outline border-0 border-b-4 text-white">Read More</button>
                 </div>
             </div>
             
